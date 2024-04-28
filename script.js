d5d7a03fd799cf4a0a751f7ccd7f21f21be70118
anton <125847709+antonbc@users.noreply.github.com.>
2024-04-28 22:08:29 +0800

    updated rps game ui

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function playGame(){
-    let playerSelection = prompt("Please enter value").toLowerCase();
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const playerScoreSpan = document.querySelector('#playerScore');
+const compScoreSpan = document.querySelector('#compScore');
+const result = document.querySelector('.result');
+
+let playerScore = 0;
+let compScore = 0;
+
+rock.addEventListener('click', () => playGame('rock'))
+paper.addEventListener('click', () => playGame('paper'))
+scissors.addEventListener('click', () => playGame('scissors'))
+
+function playGame(playerSelection){
     let choice = ['rock', 'paper', 'scissors']
     function getComputerChoice(choice){
         return choice[Math.floor(Math.random() * choice.length)];
@@ -7,18 +20,35 @@ function playGame(){
     let computerSelection = getComputerChoice(choice);
 
     function playRound(playerSelection, computerSelection) {
-        if (computerSelection == playerSelection) return "It's a tie";
-        else if (computerSelection == "scissors" && playerSelection == "paper") return "comp win";
-        else if (computerSelection == "scissors" && playerSelection == "rock") return "player win";
-        else if (computerSelection == "rock" && playerSelection == "scissors") return "comp win";
-        else if (computerSelection == "rock" && playerSelection == "paper") return "player win";
-        else if (computerSelection == "paper" && playerSelection == "rock") return "comp win";
-        else if (computerSelection == "paper" && playerSelection == "scissors") return "player win";
+        if (computerSelection === playerSelection) return "tie, no one wins"
+        else if (
+            (computerSelection === 'rock' && playerSelection === 'paper') || 
+            (computerSelection === 'paper' && playerSelection === 'scissors') || 
+            (computerSelection === 'scissors' && playerSelection === 'rock') 
+        ){
+            playerScore++;
+            playerScoreSpan.textContent = playerScore;
+            return 'player wins';
+        }
+        else{
+            compScore++;
+            compScoreSpan.textContent = compScore;
+            return 'comp wins';
+        }
     }
 
-    console.log(playRound(playerSelection, computerSelection));
-}
+    let outputText = playRound(playerSelection, computerSelection);
+    result.textContent  = outputText;
+    
+    if (playerScore === 5) {
+        result.textContent = 'game over, player win';
+        return;
+    }
+    else if (compScore === 5) {
+        result.textContent = 'game over, comp win';
+        return;
+    }
 
-for (let i = 0; i < 5; i++){
-    playGame()
+
+
 }
